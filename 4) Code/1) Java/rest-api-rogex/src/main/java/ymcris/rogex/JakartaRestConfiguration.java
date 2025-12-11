82684db1590a289f0d09cc8c94412718e0c08716
YmCris <ymcrisystems@gmail.com>
2025-12-11 13:15:35 -0600

    Creating java rest api

---
+++ 4) Code/1) Java/rest-api-rogex/src/main/java/ymcris/rogex/JakartaRestConfiguration.java
@@ -0,0 +1,19 @@
+package ymcris.rogex;
+
+import jakarta.ws.rs.ApplicationPath;
+import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.media.multipart.MultiPartFeature;
+
+/**
+ * Configures Jakarta RESTful Web Services for the application.
+ * @author Juneau
+ */
+//@ApplicationPath("resources")
+@ApplicationPath("api")
+//public class JakartaRestConfiguration extends Application{
+public class JakartaRestConfiguration extends ResourceConfig {
+    
+    public JakartaRestConfiguration() {
+        packages("ymcris.rogex").register(MultiPartFeature.class);
+    }
+}
